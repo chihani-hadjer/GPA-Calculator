acfddc9206964d4840d548a2c1b2d4bcb95904be
CHIHANI Hadjer <chihanihadjer03@gmail.com>
2026-04-04 01:14:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+function addCourse() {
+    var row = document.createElement('div');
+    row.className = 'course-row';
+
+    row.innerHTML =
+        '<input type="text" name="course[]" placeholder="Course" required>' +
+        '<input type="number" name="credits[]" placeholder="Credits" min="1" required>' +
+        '<select name="grade[]">' +
+        '<option value="4.0">A</option>' +
+        '<option value="3.0">B</option>' +
+        '<option value="2.0">C</option>' +
+        '<option value="1.0">D</option>' +
+        '<option value="0.0">F</option>' +
+        '</select>' +
+        '<button type="button" onclick="this.parentNode.remove()">Remove</button>';
+
+    document.getElementById('courses').appendChild(row);
+}
+
+function validateForm() {
+    var courses = document.getElementsByName("course[]");
+    var credits = document.getElementsByName("credits[]");
+
+    for (var i = 0; i < courses.length; i++) {
+        if (courses[i].value === "") {
+            alert("All course names required!");
+            return false;
+        }
+    }
+
+    for (var i = 0; i < credits.length; i++) {
+        if (credits[i].value <= 0) {
+            alert("Credits must be positive!");
+            return false;
+        }
+    }
+
+    return true;
+}
